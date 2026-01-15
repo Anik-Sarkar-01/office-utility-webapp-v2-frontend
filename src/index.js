f7bfd2c335391427088b5339974065a424b4e577
Anik Sarkar <sarkar.anik@duronto.tv>
2026-01-15 18:40:59 +0600

    removed / from the base url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = "https://office-utility-webapp-v2-backend.vercel.app" || "http://localhost:3000";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
